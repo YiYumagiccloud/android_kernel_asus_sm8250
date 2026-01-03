f2ef433dd579f604e327e41ebe8deae1fe57680a
YiYumagiccloud <43944978+YiYumagiccloud@users.noreply.github.com>
2026-01-03 19:34:45 +0800

    Update drm_atomic_helper.c

--- drivers/gpu/drm/drm_atomic_helper.c
+++ drivers/gpu/drm/drm_atomic_helper.c
@@ -49,7 +49,7 @@ extern bool asus_display_in_aod(void);
 extern bool asus_display_in_normal_on(void);
 /* ASUS BSP Display --- */
 
-bool g_station_pm_suspend = false;
+extern bool g_station_pm_suspend;
 EXPORT_SYMBOL(asus_current_fps);
 #endif
 
